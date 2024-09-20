df73d004c8c217dc3e86cc29d590b6802b633fa8
Petrus Breedt <petrus@global12.com>
2024-09-20 13:51:06 -0500

    Added authentication

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -5,7 +5,28 @@ import connPool from '@/app/lib/db';
 // const cp = require('@/app/lib/db');
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
- 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
